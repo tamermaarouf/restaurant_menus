687ab771646ee60818357c5b4fd467b5b8ffac5d
Tamer_Ma3rouf <tamermaarouf@gmail.com>
2023-07-05 16:53:57 +0300

    create end point

--- app.py
+++ app.py
@@ -7,28 +7,34 @@ from database_setup import app, db, MenuItem, Restaurant
 
 #------------------------> Controllers <-------------------
 @app.route('/')
-@app.route('/restaurant')
-def restaurants():
-    data = MenuItem.query.join(Restaurant).all()
-    #data = Restaurant.query.order_by(Restaurant.name).all()
-    return render_template('menu.html', restaurants=data)
+def index():
+    data = Restaurant.query.order_by(Restaurant.name).all()
+    return render_template('index.html', restaurants=data)
+
+
+@app.route('/menu/<int:restaurant_id>')
+def restaurants(restaurant_id):
+    data = MenuItem.query.filter(MenuItem.restaurant_id==restaurant_id)
+    restaurant_name = Restaurant.query.get(restaurant_id)
+    return render_template('menu.html', restaurants=data, name=restaurant_name)
 
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/menu/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
+    
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/menu/<int:restaurant_id>/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
     return "page to edit a menu item. Task 2 complete!"
 
 # Task 3: Create a route for deleteMenuItem function here
 
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/menu/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
